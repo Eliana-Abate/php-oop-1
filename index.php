c91ad42b1048b608a5a538aa22ca4c4b2c501704
Eliana-Abate <eliana.abate@outlook.it>
2021-10-05 15:43:20 +0200

    added construct

--- index.php
+++ index.php
@@ -7,5 +7,15 @@ class Movies
   public $plot;
   public $producer;
   public $year;
+
+
+    public function __construct($_title, $_genre,  $_plot, $_producer, $_year)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->plot = $_plot;
+        $this->producer = $_producer;
+        $this->year = $_year;
+    }
 }
 
